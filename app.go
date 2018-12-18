05ea9fdf70e8182fbd459dc1ee5b63ec2d60f56c
ivenabc <hopeicc@163.com>
2018-12-18 19:17:48 +0800

    first commit

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import "database/sql"
+
+// App blog app
+type App struct {
+	DB *sql.DB
+}
+
+// NewApp 生成app
+func NewApp() {
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// http.Handle(pattern, handler)
+	// http.ServeFile(w, r, name)
+	// http.ListenAndServe(addr, handler)
+}
