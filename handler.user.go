985588a1f2182924b760151bcccc99994d551221
chen demon <demon.chen@earlydata.com>
2018-12-20 23:52:47 +0800

    add create user and update user info api handler and add db handle

--- handler.user.go
+++ handler.user.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // createUserHandler 创建用户
@@ -14,8 +18,162 @@ func (a *App) createUserHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data, err := ioutil.ReadAll(ioutil.NopCloser(r.Body))
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -1,
+			ErrMessage: "read request body error",
+		})
+		return
+	}
+
+	defer r.Body.Close()
+
+	req := struct {
+		Name     string `json:"name"`
+		Password string `json:"password"`
+	}{}
+
+	err = json.Unmarshal(data, &req)
+	if err != nil {
+
+		outputJSON(w, APIStatus{
+			ErrCode:    -1,
+			ErrMessage: "json format struct error",
+		})
+		return
+	}
+
+	if req.Name == "" {
+		outputJSON(w, APIStatus{
+			ErrCode:    -2,
+			ErrMessage: "account is empty",
+		})
+		return
+	}
+
+	if req.Password == "" {
+		outputJSON(w, APIStatus{
+			ErrCode:    -3,
+			ErrMessage: "password is empty",
+		})
+		return
+	}
+
+	//查询user name 是否已存在
+
+	exist, err := a.queryUser(User{Name: req.Name})
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -4,
+			ErrMessage: "db query error",
+		})
+		return
+	}
+
+	if exist {
+		outputJSON(w, APIStatus{
+			ErrCode:    -4,
+			ErrMessage: "name is already signup",
+		})
+		return
+	}
+
+	//创建user
+	encryptedBytes, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
+	user := &User{
+		Name:              req.Name,
+		EncryptedPassword: string(encryptedBytes),
+		Status:            1,
+	}
+
+	err = a.createUser(user)
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -5,
+			ErrMessage: "create user error",
+		})
+		return
+	}
+
 	outputJSON(w, APIStatus{
 		ErrCode: 0,
 	})
 
+}
+
+func (a *App) updataUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		outputJSON(w, APIStatus{
+			ErrCode:    -1,
+			ErrMessage: "request method error",
+		})
+
+		return
+	}
+
+	data, err := ioutil.ReadAll(ioutil.NopCloser(r.Body))
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -1,
+			ErrMessage: "read request body error",
+		})
+		return
+	}
+
+	defer r.Body.Close()
+
+	req := struct {
+		ID       uint   `json:"id"`
+		Name     string `json:"name"`
+		Password string `json:"password"`
+		State    uint   `json:"state"`
+	}{}
+
+	err = json.Unmarshal(data, &req)
+	if err != nil {
+
+		outputJSON(w, APIStatus{
+			ErrCode:    -1,
+			ErrMessage: "json format struct error",
+		})
+		return
+	}
+
+	exist, err := a.queryUser(User{ID: req.ID, Name: req.Name})
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -2,
+			ErrMessage: "db query error",
+		})
+		return
+	}
+
+	if !exist {
+		outputJSON(w, APIStatus{
+			ErrCode:    -2,
+			ErrMessage: "user name is not exist",
+		})
+	}
+
+	encryptedBytes, _ := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
+	user := User{
+		ID:                req.ID,
+		Name:              req.Name,
+		EncryptedPassword: string(encryptedBytes),
+		Status:            req.State,
+	}
+
+	err = a.updateUser(user)
+	if err != nil {
+		outputJSON(w, APIStatus{
+			ErrCode:    -3,
+			ErrMessage: "update user info error",
+		})
+		return
+	}
+
+	outputJSON(w, APIStatus{
+		ErrCode: 0,
+	})
 }
--- logic.db.user.go
+++ logic.db.user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 // createUser 创建用户
@@ -25,4 +26,87 @@ func (a *App) createUser(u *User) (err error) {
 	u.ID = uint(id)
 
 	return
+}
+
+func (a *App) queryUser(u User) (result bool, err error) {
+	result = true
+
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	db := a.getDB()
+
+	var rows *sql.Rows
+	if u.ID != 0 {
+		rows, err = db.Query(fmt.Sprintf("SELECT id users WHERE id=%d", u.ID))
+	} else if u.Name != "" {
+		rows, err = db.Query(fmt.Sprintf("SELECT id users WHERE name=%s", u.Name))
+	}
+
+	if err != nil {
+		return result, err
+	}
+
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			result = false
+			return result, err
+		}
+
+		if id == 0 {
+			result = false
+			return result, err
+		}
+	}
+
+	return result, err
+}
+
+func (a *App) updateUser(u User) error {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	db := a.getDB()
+
+	sqlStr := "UPDATE users"
+	updateStr := " SET "
+	conditionStr := " WHERE "
+
+	if u.ID != 0 {
+		conditionStr = fmt.Sprintf("%sid=%d", conditionStr, u.ID)
+		if u.Name != "" {
+			updateStr = fmt.Sprintf("%sname=%s", updateStr, u.Name)
+		}
+
+		if u.EncryptedPassword != "" {
+			updateStr = fmt.Sprintf("%s,encrypted_password=%s", updateStr, u.EncryptedPassword)
+		}
+
+		// if u.Status != 0 {
+		// 	updateStr = fmt.Sprintf("%s,status=%d", updateStr, u.Status)
+		// }
+	} else if u.Name != "" {
+		conditionStr = fmt.Sprintf("%sname=%s", conditionStr, u.Name)
+		if u.EncryptedPassword != "" {
+			updateStr = fmt.Sprintf("%sname=%s", updateStr, u.Name)
+
+		}
+
+		// if u.Status != 0 {
+		// 	updateStr = fmt.Sprintf("%s,status=%d", updateStr, u.Status)
+		// }
+	}
+
+	if updateStr == " SET " || conditionStr == " WHERE " {
+		return fmt.Errorf("update info error or update condition info error")
+	}
+
+	sqlStr = fmt.Sprintf("%s%s%s", sqlStr, updateStr, conditionStr)
+
+	_, err := db.Exec(sqlStr)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
