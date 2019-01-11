231adecb23cdf21058f66e33096c2cfeb9faf3b5
chen demon <demon.chen@earlydata.com>
2019-01-12 02:42:11 +0800

    add create post

--- logic.go
+++ logic.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // DEBUG debug
@@ -26,4 +28,15 @@ func outputJSON(w http.ResponseWriter, data interface{}) {
 		http.Error(w, "500", http.StatusInternalServerError)
 		return
 	}
+}
+
+func getRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
