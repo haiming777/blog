c04b05b7a1ca014846d94a6d23ca067070138888
chen demon <demon.chen@earlydata.com>
2019-01-13 00:42:48 +0800

    add query categories by parent id

--- logic.db.category.go
+++ logic.db.category.go
@@ -64,3 +64,34 @@ func (a *App) queryCategoryByID(c *Category) error {
 	return db.QueryRow("SELECT * FROM categories WHERE id=?", c.ID).
 		Scan(&c.ID, &c.Name, &c.ParentID)
 }
+
+func (a *App) queryCategoriesByParentID(pid uint) ([]Category, error) {
+	db := a.getDB()
+
+	a.mutex.RLock()
+	defer a.mutex.RUnlock()
+
+	var (
+		id   uint
+		name string
+	)
+
+	rows, err := db.Query("SELECT id,name FROM categories WHERE parent_id=?", pid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := make([]Category, 0)
+	for rows.Next() {
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			return nil, err
+		}
+
+		category := Category{ID: id, Name: name, ParentID: pid}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
